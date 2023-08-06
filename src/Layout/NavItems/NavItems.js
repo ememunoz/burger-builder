993aeb4d860fb471e053181feec3e78958cee7f4
Eme Muñoz <github@eme.dev>
2023-08-06 17:01:26 -0400

    fix build

--- src/Layout/NavItems/NavItems.js
+++ src/Layout/NavItems/NavItems.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classes from './NavItems.module.css';
-import NavItem from './NavItem/NavItem';
+import React from 'react'
+
+import NavItem from './NavItem/NavItem'
+import classes from './NavItems.module.css'
 
 const NavItems = props => {
   return (
     <ul className={classes.navItems}>
-      <NavItem href='/' active>Burger Builder</NavItem>
-      <NavItem href='/'>Checkout</NavItem>
+      <NavItem href="/" active>
+        Burger Builder
+      </NavItem>
+      <NavItem href="/">Checkout</NavItem>
     </ul>
   )
-};
-
-NavItems.propTypes = {
 }
 
-export default NavItems;
+export default NavItems
